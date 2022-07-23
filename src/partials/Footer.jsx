be24cfbfbb29e6b28209bac3194995adf4df46ae
David Martinez <david.mtz00@hotmail.com>
2022-07-22 19:10:57 -0500

    Add links

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -130,9 +130,11 @@ function Footer() {
           {/* Social links */}
           <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
             <li>
-              <Link to="#" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
-                <img src={InstagramSvg} alt="Instagram" className="w-8 h-8" />
-              </Link>
+              {/* <Link to="#" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter"> */}
+                <a href="https://www.instagram.com/lifeskills.utd/">
+                  <img className="w-auto h-8" src={InstagramSvg} alt="Instagram" />
+                </a>
+              {/* </Link> */}
             </li>
             {/* <li className="ml-4">
               <Link to="#" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Github">
