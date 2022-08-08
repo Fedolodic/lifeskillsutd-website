e9392ed934947fc87479d503b7d78fbc19fca61a
David Martinez <david.mtz00@hotmail.com>
2022-08-07 20:06:16 -0500

    Add Zainab

--- src/partials/Testimonials.jsx
+++ src/partials/Testimonials.jsx
@@ -4,6 +4,7 @@ import TestimonialImage from '../images/testimonial.jpg';
 import KarenImg from '../images/Karen.jpg';
 import AbiramiImg from '../images/Abirami.jpg';
 import RakshakImg from '../images/Rakshak.jpg';
+import ZainabImg from '../images/Zainab.jpg';
 
 function Testimonials() {
   return (
@@ -86,16 +87,15 @@ function Testimonials() {
                   <svg className="absolute top-0 right-0 -mt-3 -mr-8 w-16 h-16 fill-current text-blue-500" viewBox="0 0 64 64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
                     <path d="M37.89 58.338c-2.648-5.63-3.572-10.045-2.774-13.249.8-3.203 8.711-13.383 23.737-30.538l2.135.532c-6.552 10.033-10.532 17.87-11.939 23.515-.583 2.34.22 6.158 2.41 11.457l-13.57 8.283zm-26.963-6.56c-2.648-5.63-3.572-10.046-2.773-13.25.799-3.203 8.71-13.382 23.736-30.538l2.136.533c-6.552 10.032-10.532 17.87-11.94 23.515-.583 2.339.22 6.158 2.41 11.456l-13.57 8.283z" />
                   </svg>
-                  <img className="relative rounded-full" src={TestimonialImage} width="96" height="96" alt="Testimonial 01" />
+                  <img className="relative rounded-full" src={ZainabImg} width="96" height="96" alt="Testimonial 01" />
                 </div>
                 <blockquote className="text-xl font-medium mb-4">
-                  “ Why did you join LifeSkillsUTD. Insert your major. What workshop/event are you most excited for. Fun hobby or fun fact. “
+                  “ I’m a Junior majoring in Neuroscience and minoring in Public Health. I wanted to start this club so people like could be incentivized to learn new skills. I’m super excited for the workshops and socials we have planned this year, mainly the ones involving meal prep. Fun fact about me, I love trying new foods! “
                 </blockquote>
-                <cite className="block font-bold text-lg not-italic mb-1">FirtName LastName</cite>
+                <cite className="block font-bold text-lg not-italic mb-1">Zainab Gangardiwala</cite>
                 <div className="text-gray-600">
-                  <span>Officer position</span>
+                <span>Founder & Co-President</span> <a className="text-blue-600 hover:underline" href="https://www.instagram.com/lifeskills.utd/">@LifeSkillsUTD</a>
                 </div>
-                <a className="text-blue-600 hover:underline" href="#0">@YourSocial</a>
               </div>
 
             </div>
